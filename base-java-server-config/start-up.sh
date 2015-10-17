c055cf19cf9d2627ceb4b7d7a59b13410eb2a4fd
David Marin <davidokun99@gmail.com>
2015-10-17 11:51:44 -0500

    Update Dokcer configuration

--- base-java-server-config/start-up.sh
+++ base-java-server-config/start-up.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker run -d -p 27017:27017 --name mongo-server davidokun/centos7-db-server
-docker run -d -p 8080:8080 -p 8787:8787 -p 9990:9990 --name wildfly-server --link mongo-server:mongo-server davidokun/centos7-app-server
-docker run -d -p 9000:9000 -p 9092:9092 --name sonar-server davidokun/centos7-sonar-server
+docker run -d -p 27017:27017 --name mongo-server david/centos7-db-server
+docker run -d -p 8080:8080 -p 8787:8787 -p 9990:9990 --name wildfly-server --link mongo-server:mongo-server david/centos7-app-server
+docker run -d -p 9000:9000 -p 9092:9092 --name sonar-server david/centos7-sonar-server
